6725f3c1df5abffd436f0fd0fc9e6249d4901536
crusiris <cruzsirys@gmail.com>
2020-04-24 12:01:37 -0400

    Añadiendo componentes de estilos al componente principal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import imagen from './cryptomonedas.png';
 
+const Container = styled.div`
+  max-width:900px;
+  margin:0 auto;
+    @media (min-width:992px){
+      display:grid;
+      grid-template-columns:repeat(2,1fr);
+      column-gap:2rem;
+    }
+`;
+
+const Image = styled.img`
+  max-width:100%;
+  margin-top:5rem;
+`;
+
+const Heading = styled.h1`
+  font-family:'Bebas Neue', cursive;
+  color:#FFF;
+  text-align:left;
+  font-weight:700;
+  font-size:50px;
+  margin-bottom:50px;
+  margin-top:80px;
+
+    &::after {
+      content:'';
+      width:100px;
+      height:6px;
+      background-color:#66A2FE;
+      display:block;
+    }
+`;
 
 function App() {
   return (
+    <Container>
     <div>
-     <h1>esto es un hola mundo</h1>
+      <Image
+      src={imagen} alt="imagen criptomoneda"
+      />
     </div>
+    <div>
+      <Heading>Cotiza Criptomonedas al Instante</Heading>
+    </div>
+    </Container>
   );
 }
 
