aac7e9ef4a687db37472646875cd63d9246ab20a
crusiris <cruzsirys@gmail.com>
2020-04-24 15:52:17 -0400

    Aplicando styled component a nuestro hooks

--- src/hooks/useCurrency.js
+++ src/hooks/useCurrency.js
@@ -1,4 +1,26 @@
 import React, { Fragment, useState } from 'react';
+import styled from '@emotion/styled';
+
+const Label = styled.label`
+font-family:'Bebas Neue', cursive;
+color:#FFF;
+text-transform:uppercase;
+font-weight:bold;
+font-size:2.4rem;
+margin-top:2rem;
+display:block;
+`;
+
+const Select = styled.select`
+width:100%;
+display:block;
+padding:1rem;
+-webkit-appearance:none;
+border-radius:10px;
+border:none;
+font-size:1.2rem;
+
+`;
 
 const useCurrency = (label, stateInitial, optionsCurrency) => {
 
@@ -8,16 +30,16 @@ const useCurrency = (label, stateInitial, optionsCurrency) => {
     const toSelect = ()=>(
 
         <Fragment>
-            <label>{label}</label>
-            <select
+            <Label>{label}</Label>
+            <Select
             onChange={e => updateState(e.target.value)}
             value={state}
             >
-                <option value="">---Seleccione</option>
+                <option value=""> --- Seleccione Moneda ---</option>
                 {optionsCurrency.map(option =>(
                     <option key={option.code} value={option.code}>{option.name}</option>
                 ))}
-            </select>
+            </Select>
         </Fragment>
 
     );
